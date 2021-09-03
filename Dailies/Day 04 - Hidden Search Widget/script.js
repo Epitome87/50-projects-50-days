4e74edfa96be6a092cf3e0b682b37e3cf5592c15
Matthew McGrath <rubywep@hotmail.com>
2021-09-03 09:49:53 -0700

    Delete Dailies directory

--- Dailies/Day 04 - Hidden Search Widget/script.js
+++
@@ -1,9 +0,0 @@
-const search = document.querySelector('.search');
-const input = document.querySelector('.input');
-const button = document.querySelector('.btn');
-
-button.addEventListener('click', (event) => {
-  // Expand the Search Input by giving it "active" class
-  search.classList.toggle('active');
-  input.focus();
-});
